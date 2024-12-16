77424e1c53968ee7508a5958feb559ee92e438ab
leechan <dlcks0601@gmail.com>
2024-12-16 14:33:13 +0900

    add code

---
+++ index.js
@@ -0,0 +1,4 @@
+export { checkedArr } from './src/checkedArr';
+export { findOddNums } from './src/findOddNum';
+export { findEvenNums } from './src/findEvenNums';
+export { capitalize } from './src/capitalize';
